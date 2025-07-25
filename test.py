68a1ff8591f9b0494beac65f59efd03a7c9ecff7
yash0patel <yashpatelrupal@gmail.com>
2025-07-25 15:06:56 +0530

    Initial Django project with main app

---
+++ test.py
@@ -0,0 +1,11 @@
+import pandas
+import numpy
+import yfinance
+import nltk
+import textblob
+import vaderSentiment
+import sklearn
+import requests
+import matplotlib
+import seaborn
+print("All core libraries imported successfully!")
